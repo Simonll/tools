c02003185a0aa2cc4f4189c7156bf6184de3a477
sll <evol.simon@gmail.com>
2017-06-15 20:20:19 -0400

    Now possible to extract MCMC pt from MS and MSAA models

--- extract_tree.py
+++ extract_tree.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 import sys
-import os 
+import os
 import csv
 import re
 import math
@@ -16,123 +16,235 @@ if (len(sys.argv) < 4) :
     print("tree : ", "phylogenetic tree ")
     print("tl : "  , "tree length ")
     sys.exit(0)
-    
-inputfile  = sys.argv[1]
-index  = int(sys.argv[2])
-outputfile = sys.argv[3]
-param = sys.argv[4]
+
+model = sys.argv[1]
+inputfile  = sys.argv[2]
+index  = int(sys.argv[3])
+outputfile = sys.argv[4]
+param = sys.argv[5]
 Nparam = len(sys.argv)
 
 def readChainMSAA(pathtochain,index):
-   
+
     chaintoread = open(pathtochain, "r")
     lines = chaintoread.readlines()
     chaintoread.close()
     print("path to chain " , pathtochain, " iteration to recover ", index, "number of lines in chain file ",  len(lines))
-    
-    pt= 0 
+
+    pt= 0
     k = 0
     brack = 0
-    start = 0 
+    start = 0
     end = 0
     a = True
     b = 0
     new_start = 0
-    old_start = 0 
+    old_start = 0
+    step =  0
 
     for line in lines:
+        #print(pt)
         if (line.startswith("(") and brack == 0 and pt == index):
             start = k
             brack = 1
-            
+
         if (line.startswith("(") and brack == 1 and pt == index+1):
             end = k
-            
-        if end != 0 : 
+
+        if end != 0 :
             break
-            
+
         if (line.startswith("(")):
             pt+= 1
-            
+
         if (line.startswith("(") and k != 0 and a == True):
             step = k
-            a = False 
+            a = False
             print("expected step size (base on first iteration) : ", step)
             print("only inconsitent iterations will be shown")
-        
+
         if (line.startswith("(") and pt > 1):
             old_start = new_start
             new_start = k
             if (step != (new_start-old_start)):
                 print("inconsitent chain ","pt ",pt," expected step size ", step," new step size " ,new_start-old_start, " start ", old_start, " end ",new_start)
-                
-            
+
+
         k+=1
+
     if(pt < index):
         print("faulty chain")
         sys.exit(0)
-    
+
     lines = lines[start:end]
-    
-#     ssaaprofiles = None #np.matrix
-#     allocations = None #np.array
-    
+
+    ssaaprofiles = None #np.matrix
+    allocations = None #np.array
+
     tree = lines[0]
     tl = extrac_treelenght(tree)
-    
-#     nucp = np.array(lines[3].strip().split("\t"),dtype=float)
-    
-#     nucrr = np.array(lines[5].strip().split("\t"),dtype=float)
-    
-#     codonprofile = np.array(lines[7].strip().split("\t"),dtype=float)
-    
-#     omega = float(lines[9])
-    
-#     Nsite_codon = int(lines[11])
-    
-    
-#     ssaaprofiles_list = [line.strip().split("\t") for line in lines[14:Nsite_codon+14]]
-#     ssaaprofiles = np.array(ssaaprofiles_list,dtype=float)
-    
-#     allocations =  np.array(lines[Nsite_codon+14].strip().split("\t"),dtype=float)
-    
-    
-    dic = {} 
+    L1  = lines[1]
+    L2  = lines[2]
+    nucp = np.array(lines[3].strip().split("\t"),dtype=float)
+    L4  = lines[4]
+    nucrr = np.array(lines[5].strip().split("\t"),dtype=float)
+    L6  = lines[6]
+    codonprofile = np.array(lines[7].strip().split("\t"),dtype=float)
+    L8  = lines[8]
+    omega = float(lines[9])
+    L10 = lines[10]
+    Nsite_codon = int(lines[11])
+    L12 = np.array(lines[12].strip().split("\t"),dtype=float)
+    L13 = lines[13]
+    ssaaprofiles_list = [line.strip().split("\t") for line in lines[14:Nsite_codon+14]]
+    ssaaprofiles = np.array(ssaaprofiles_list,dtype=float)
+
+    allocations =  np.array(lines[Nsite_codon+14].strip().split("\t"),dtype=float)
+
+
+    dic = {}
+    dic["iter"] = lines
+    dic["tl"] = tl
     param = ["tree","tl","nucp","nucrr","codonprofile","omega","Nsite_nuc","ssaaprofiles","allocations","iter"]
     dic["tree"] = tree
-    dic["tl"] = tl
-    dic["iter"] = lines
-#     dic["nucp"] = nucp
-#     dic["nucrr"] = nucrr
-#     dic["codonprofile"] = codonprofile
-#     dic["omega"] = omega
-#     dic["Nsite_codon"] = Nsite_codon
-#     dic["ssaaprofiles"] = ssaaprofiles
-#     dic["allocations"] = allocations
+    dic["L1"] = L1
+    dic["L2"] = L2
+    dic["nucp"] = nucp
+    dic["L4"] = L4
+    dic["nucrr"] = nucrr
+    dic["L6"] = L6
+    dic["codonprofile"] = codonprofile
+    dic["L8"] = L8
+    dic["omega"] = omega
+    dic["L10"] = L10
+    dic["Nsite_codon"] = Nsite_codon
+    dic["L12"] = L12
+    dic["L13"] = L13
+    dic["ssaaprofiles"] = ssaaprofiles
+    dic["allocations"] = allocations
     return dic
+
+def readChainMS(pathtochain,index):
+
+    chaintoread = open(pathtochain, "r")
+    lines = chaintoread.readlines()
+    chaintoread.close()
+    print("path to chain " , pathtochain, " iteration to recover ", index, "number of lines in chain file ",  len(lines))
+
+    pt= 0
+    k = 0
+    brack = 0
+    start = 0
+    end = 0
+    a = True
+    b = 0
+    new_start = 0
+    old_start = 0
+    step =  0
+
+    for line in lines:
+        #print(pt)
+        if (line.startswith("(") and brack == 0 and pt == index):
+            start = k
+            brack = 1
+
+        if (line.startswith("(") and brack == 1 and pt == index+1):
+            end = k
+
+        if end != 0 :
+            break
+
+        if (line.startswith("(")):
+            pt+= 1
+
+        if (line.startswith("(") and k != 0 and a == True):
+            step = k
+            a = False
+            print("expected step size (base on first iteration) : ", step)
+            print("only inconsitent iterations will be shown")
+
+        if (line.startswith("(") and pt > 1):
+            old_start = new_start
+            new_start = k
+            if (step != (new_start-old_start)):
+                print("inconsitent chain ","pt ",pt," expected step size ", step," new step size " ,new_start-old_start, " start ", old_start, " end ",new_start)
+
+
+        k+=1
+
+    if(pt < index):
+        print("faulty chain")
+        sys.exit(0)
+
+    lines = lines[start:end]
+
+    ssaaprofiles = None #np.matrix
+    allocations = None #np.array
+
+    tree = lines[0]
+    tl = extrac_treelenght(tree)
+    L1  = lines[1]
+    L2  = lines[2]
+    nucp = np.array(lines[3].strip().split("\t"),dtype=float)
+    L4  = lines[4]
+    nucrr = np.array(lines[5].strip().split("\t"),dtype=float)
+    L6  = lines[6]
+    codonprofile = np.array(lines[7].strip().split("\t"),dtype=float)
+    L8  = lines[8]
+    omega = float(lines[9])
+    #L10 = lines[10]
+    Nsite_codon = int(lines[10])
+    L12 = np.array(lines[11].strip().split("\t"),dtype=float)
+    L13 = lines[12]
+    ssaaprofiles_list = np.array(lines[13].strip().split("\t"),dtype=float)
+    allocations =  np.array(lines[14].strip().split("\t"),dtype=float)
+    param = ["tree","tl","nucp","nucrr","codonprofile","omega","Nsite_nuc","ssaaprofiles","allocations","iter"]
+    dic = {}
+    dic["iter"] = lines
+    dic["tl"] = tl
+    dic["tree"] = tree
+    dic["L1"] = L1
+    dic["L2"] = L2
+    dic["nucp"] = nucp
+    dic["L4"] = L4
+    dic["nucrr"] = nucrr
+    dic["L6"] = L6
+    dic["codonprofile"] = codonprofile
+    dic["L8"] = L8
+    dic["omega"] = omega
+    #dic["L10"] = L10
+    dic["Nsite_codon"] = Nsite_codon
+    dic["L12"] = L12
+    dic["L13"] = L13
+    dic["ssaaprofiles"] = ssaaprofiles
+    dic["allocations"] = allocations
+    return dic
+
+
 
 def extrac_treelenght(tree):
         t = Tree(tree)
         s = np.array([node.dist for node in t.traverse("postorder") ])
         s = s.sum()
-        return s 
-        
+        return s
+
 if __name__ == '__main__':
-    
-    
+
     pathtochain = inputfile + ".chain"
-    print(pathtochain,index)                       
-    dic = readChainMSAA(pathtochain,index)
-    
-    outfile =  open(outputfile,"w")
+    print(model,pathtochain,index)
+    dic {}
+    if (model = "MSAA") :
+        dic = readChainMSAA(pathtochain,index)
+    elif (model == "MS") :
+        dic = readChainMS(pathtochain,index)
+
+    outfile =  open(outputfile,"a")
+
     if (param in dic):
-        if (param == "tree" or param == "tl" ): 
-            outfile.writelines(str(dic[param])+"\n")
-        if (param == "iter"):
-            outfile.writelines(dic[param])
-    else : 
+        if (param == "tree" or param == "tl" or param == "codonprofile" ):
+            outfile.writelines(str(dic[param]))
+    elif (param == "extrac"):
+        outfile.writelines(dic["iter"])
+
+    else :
         print("unknown param : ", param)
-    
-    
-    
-    
